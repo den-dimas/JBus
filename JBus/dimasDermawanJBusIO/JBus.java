030e11bfa34494a54b45856e77202dfe1578d501
Dimas <dimas.dermawan@ui.ac.id>
2023-09-15 10:52:18 +0700

    CS02 - Menghapus method-method CS01 pada Class JBus

--- JBus/dimasDermawanJBusIO/JBus.java
+++ JBus/dimasDermawanJBusIO/JBus.java
@@ -14,57 +14,9 @@ public class JBus
     }
 
     public static Bus createBus() {
-        Price price = new Price(0, 0);
+        Price price = new Price(100000, 50);
         Bus bus = new Bus("Dermawan's Bus", Facility.AC, price, 56);
         
         return bus;
-    }
-    
-    public static int getBusId() {
-        return 0;
-    }
-    
-    public static String getBusName() {
-        return "Bus";
-    }
-    
-    public static boolean isDiscount() {
-        return true;
-    }
-    
-    public static float getDiscountPercentage(int beforeDiscount, int afterDiscount) {
-        if (beforeDiscount < afterDiscount) {
-            return 0.0f;
-        }
-        else if (beforeDiscount == 0) {
-            return 0.0f;
-        }
-        else {
-            return ((beforeDiscount - afterDiscount) / (float) beforeDiscount) * 100;
-        }
-    }
-    
-    public static int getDiscountedPrice(int price, float discountPercentage) {
-        if (discountPercentage > 100) {
-            return 0;
-        } else {
-            return (int) ((1 - (discountPercentage / 100)) * price);   
-        }
-    }
-    
-    public static int getOriginalPrice(int discountedPrice, float discountPercentage) {
-        return (int) (discountedPrice / (1 - (discountPercentage / 100)));
-    }
-    
-    public static float getAdminFeePercentage() {
-        return 0.05f;
-    }
-    
-    public static int getAdminFee(int price) {
-        return (int) (price * getAdminFeePercentage());
-    }
-    
-    public static int getTotalPrice(int price, int numberOfSeat) {
-        return price * numberOfSeat + getAdminFee(price * numberOfSeat);
     }
 }
