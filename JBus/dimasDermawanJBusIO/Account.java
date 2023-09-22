1736d0d5ea471dedabaa140399a4c74fd71c4f49
Dimas <dimas.dermawan@ui.ac.id>
2023-09-22 11:24:34 +0700

    CS03 - Benerin extends Payment class dari Serializable jadi Invoice

--- JBus/dimasDermawanJBusIO/Account.java
+++ JBus/dimasDermawanJBusIO/Account.java
@@ -5,6 +5,14 @@ public class Account extends Serializable {
     public String name;
     public String password;
     
+    /**
+     * Melakukan pembuatan akun.
+     * 
+     * @param id ID akun
+     * @param name Nama akun
+     * @param email Email akun
+     * @param password Password akun
+     */
     public Account(int id, String name, String email, String password) {
         super(id);
         
--- JBus/dimasDermawanJBusIO/Payment.java
+++ JBus/dimasDermawanJBusIO/Payment.java
@@ -1,13 +1,13 @@
 package dimasDermawanJBusIO;
 
-public class Payment extends Serializable {
+public class Payment extends Invoice {
     private int busId;
     
     public String departureDate;
     public String busSeat;
     
     public Payment(int id, int buyerId, int renterId, String time, int busId, String departureDate, String busSeat) {
-        super(id);
+        super(id, buyerId, renterId, time);
         
         this.busId = busId;
         this.departureDate = departureDate;
@@ -15,7 +15,7 @@ public class Payment extends Serializable {
     }
     
     public Payment(int id, Account buyer, Renter renter, String time, int busId, String departureDate, String busSeat) {
-        super(id);
+        super(id, buyer, renter, time);
         
         this.busId = busId;
         this.departureDate = departureDate;
@@ -23,7 +23,7 @@ public class Payment extends Serializable {
     }
     
     public String print() {
-        return "Bus ID : " + this.getBusId() + "\nTanggal Berangkat : " + this.departureDate + "\nKode Bangku : " + this.busSeat + "\n";
+        return "Payment ID : " + this.id + "\nBuyer ID : " + this.buyerId + "\nRenter ID : " + this.renterId + "\nTime : " + this.time + "\nBus ID : " + this.getBusId() + "\nTanggal Berangkat : " + this.departureDate + "\nKode Bangku : " + this.busSeat + "\n";
     }
     
     public int getBusId() {
