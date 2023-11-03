e88b866b63079249e4f3c9afe5f916785bb05ba5
Dimas <dimas.dermawan@ui.ac.id>
2023-11-03 11:29:03 +0700

    CS06 - Menyelesaikan CS06

--- JBus/src/dimasDermawanJBusIO/JBus.java
+++ JBus/src/dimasDermawanJBusIO/JBus.java
@@ -4,16 +4,28 @@ import java.util.List;
 
 public class JBus {
     public static void main(String[] args) {
+        JsonTable<Bus> busList = null;
+
         try {
-            String filepath = "C:\\Users\\Den\\Documents\\_perkuliahan\\Semester 3\\Pemrograman Berorientasi Objek dan Praktikum\\Praktikum\\JBus\\data\\buses.json";
-            JsonTable<Bus> busList = new JsonTable<>(Bus.class, filepath);
-            List<Bus> filteredBus = filterByDeparture(busList, City.JAKARTA, 1, 10);
-            filteredBus.forEach(bus -> System.out.println(bus.toString()));
+            String filepath = "C:\\Users\\Den\\Documents\\_perkuliahan\\Semester 3\\Pemrograman Berorientasi Objek dan Praktikum\\Praktikum\\JBus\\data\\buses_CS.json";
+            busList = new JsonTable<>(Bus.class, filepath);
         } catch (Throwable t) {
             t.printStackTrace();
         }
+
+//        List<Bus> filteredBus = filterByDeparture(busList, City.JAKARTA, 0, 3);
+//        filteredBus.forEach(bus -> System.out.println(bus.toString()));
+
+//        List<Bus> priceBus = filterByPrice(busList, 100000, 500000);
+//        priceBus.forEach(bus -> System.out.println(bus.toString()));
+
+//        Bus busId = filterBusId(busList, 155);
+//        System.out.println(busId.toString());
+
+        List<Bus> depArrBus = filterByDepartureAndArrival(busList, City.JAKARTA, City.SURABAYA, 0, 3);
+        depArrBus.forEach(bus -> System.out.println(bus.toString()));
     }
-    
+
     public static Bus createBus() {
         Price price = new Price(10000, 0);
         Station depok = new Station("DEPOK", City.DEPOK, "DEPOK");
@@ -24,5 +36,17 @@ public class JBus {
 
     public static List<Bus> filterByDeparture(List<Bus> buses, City departure, int page, int pageSize) {
         return Algorithm.paginate(Algorithm.collect(buses.toArray(new Bus[0]), bus -> bus.city == departure), page, pageSize, t -> true);
+    }
+
+    public static List<Bus> filterByPrice(List<Bus> buses, int min, int max) {
+        return Algorithm.collect(buses.toArray(new Bus[0]), bus -> bus.price.price >= min && bus.price.price <= max);
+    }
+
+    public static Bus filterBusId(List<Bus> buses, int id) {
+        return Algorithm.find(buses.toArray(new Bus[0]), bus -> bus.id == id);
+    }
+
+    public static List<Bus> filterByDepartureAndArrival(List<Bus> buses, City departure, City arrival, int page, int pageSize) {
+        return Algorithm.paginate(Algorithm.collect(buses.toArray(new Bus[0]), bus -> bus.departure.city == departure && bus.arrival.city == arrival), page, pageSize, t -> true);
     }
 }
--- JBus/src/dimasDermawanJBusIO/Renter.java
+++ JBus/src/dimasDermawanJBusIO/Renter.java
@@ -1,9 +1,15 @@
 package dimasDermawanJBusIO;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class Renter extends Serializable {
     public String address;
     public String companyName;
     public int phoneNumber;
+
+    private final String REGEX_PHONE = "([0-9])\\d{9,12}";
+    private final String REGEX_NAME = "^[A-Z][a-zA-Z0-9_]\\w{4,20}";
     
     public Renter(String companyName) {
         this.companyName = companyName;
@@ -27,5 +33,16 @@ public class Renter extends Serializable {
         this.companyName = companyName;
         this.phoneNumber = phoneNumber;
         this.address = address;
+    }
+
+    public boolean validate() {
+        Pattern patName = Pattern.compile(REGEX_NAME);
+        Pattern patPhone = Pattern.compile(REGEX_PHONE);
+
+        if (patName.matcher(companyName).find() && patPhone.matcher(Integer.toString(phoneNumber)).find()) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
